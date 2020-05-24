30613405ce30306fba8638f8164ea043a2b9c702
Nishant763 <bhatianishant763@gmail.com>
2020-05-24 13:42:42 +0530

    Rendered html to the browser,installed nodemon,ejs

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(express.static('assets'));
 app.use(express.urlencoded());
 
+app.set('view engine','ejs');
+
+app.get('/', function(req,res){
+    return res.render('home');
+})
 
 app.listen(port, function(err){
     if(err){
