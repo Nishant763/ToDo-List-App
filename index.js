916970ce9d85a07f38478dc102da7718f2b10264
Nishant763 <bhatianishant763@gmail.com>
2020-05-24 14:04:06 +0530

    Started with the design using html and css

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use(express.static('assets'));
+app.use(express.static('views'));
 app.use(express.urlencoded());
 
 app.set('view engine','ejs');
